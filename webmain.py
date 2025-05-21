076cb244299ffdade35f159d60be4abdda4fa162
Evan Metzinger <55550077+BiscottiMuncher@users.noreply.github.com>
2025-05-21 00:30:16 -0700

    Update webmain.py
    Added text editor, and some other arbitrary functions

--- webmain.py
+++ webmain.py
@@ -1,25 +1,56 @@
 ## Pigpen webmain
 # Controls snorthandler bash script
 
-from flask import Flask, redirect, render_template, request
-import subprocess, socket
+from flask import Flask, redirect, render_template, request, Response
+import subprocess, socket, os, time
 
 
 ## Start Snortpipe process immedualty
 def startSnortPipe():
-         subprocess.run(["snortpipe.sh &"], shell=True)
+         subprocess.run(["/root/scripts/pigpen/project/snortpipe.sh &"], shell=True)
 
-## Path to Socket on local machine
+# Very crude view file contents
+def viewFileContents(filePath):
+        with open(filePath, "r") as f:
+                content = f.read()
+        return Response (content, mimetype='text/plain')
+
+## Path to Socket on local machine, this is needef for all comms between snortpipe and webmain
 socketPath = "/tmp/snortSock.sock"
-
 
 ## Global Snort Running Variable
 isSnort = 0
 
+### Internal commands are for when there is no web request
+## Internal Kill Snort commant
+def intKillSnort():
+        global isSnort
+        if isSnort == 1:
+                with socket.socket(socket.AF_UNIX, socket.SOCK_STREAM) as client:
+                        client.connect(socketPath)
+                        client.sendall(b"kill\n")
+                        isSnort = 0
+        else:
+                print("Snort Not Running")
+        return redirect('/')
+
+##Internal Start Snort func
+def intStartSnort():
+        global isSnort
+        print(f"Snort Status: {isSnort}")
+        if isSnort == 0:
+                with socket.socket(socket.AF_UNIX, socket.SOCK_STREAM) as client:
+                        client.connect(socketPath)
+                        client.sendall(b"start\n")
+                        isSnort = 1
+        else:
+                 print("Snort Already Running")
+        return redirect('/')
+
 # DEfine flask app name
 app = Flask(__name__)
 
-## Path to main page, holds all control 
+## Path to main page, holds most down low cotnrol
 @app.route('/')
 def main():
         return render_template('index.html')
@@ -38,11 +69,6 @@ def startSnort():
                                 client.connect(socketPath)
                                 client.sendall(b"start\n")
                                 isSnort = 1
-                                # Added in Response Code
-#                               response = client.recv(1024).decode()
-#                               print(response)
-#                               if response == "!> STARTED":
-#                                       isSnort = 1
         else:
                 print("Snort Already Running")
 
@@ -75,6 +101,63 @@ def killHandler():
                         client.sendall(b"quit\n")
         return redirect('/')
 
+
+## Reload route
+# Used when you just need to quickly restart snort, upates to files, and so on
+@app.route('/reload', methods=['POST'])
+def reloadSnort():
+        if 'reload' in request.form:
+                intKillSnort()
+                print("Reloading Snort")
+                time.sleep(5)
+                print("Starting Snort")
+                intStartSnort()
+        return redirect('/')
+
+### Break for New Dev ###
+
+## Ideas
+# -Edit text file in browser
+# Need to load file and get contents (rw+)
+# Give environment to actually edit text (No idea how, some flask BS)
+# Save file back to machine
+# Restart Snort with new file automatically?
+
+
+## Crude view Local Rules
+@app.route('/viewrules')
+def viewRules():
+        return viewFileContents('/usr/local/etc/rules/local.rules')
+
+## Crude view Current Config
+@app.route('/viewconfig')
+def viewConfig():
+        return viewFileContents('/usr/local/etc/snort/snort.lua')
+
+## View defined log
+@app.route('/viewlog')
+def viewLog():
+        return viewFileContents('/var/log/snort/alert_json.txt')
+
+## Shitty text editor
+@app.route('/edit', methods=['GET', 'POST'])
+def editFile():
+    fileToEdit = request.args.get('file')
+    if not fileToEdit or not os.path.isfile(fileToEdit):
+        return "Invalid file path", 406
+
+    if request.method == 'POST':
+        content = request.form.get('content')
+        with open(fileToEdit, 'w') as fp:
+            fp.write(content)
+        redirect_url = f'/edit?file={fileToEdit}'
+        return redirect(redirect_url)
+
+    # GET method - load file content
+    with open(fileToEdit, 'r') as fp:
+        content = fp.read()
+
+    return render_template('editor.html', content=content, fileToEdit=fileToEdit)
 
 ## Main function
 if __name__ == '__main__':
