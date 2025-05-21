838d83145c008a05ccce98adc6e107a322093f7a
Evan Metzinger <55550077+BiscottiMuncher@users.noreply.github.com>
2025-05-21 13:10:17 -0700

    v0.3.0-Alpha First Commit
    Mainly added text editor, files manager, and some smaller things that work with templates

--- webmain.py
+++ webmain.py
@@ -18,8 +18,21 @@ def viewFileContents(filePath):
 ## Path to Socket on local machine, this is needef for all comms between snortpipe and webmain
 socketPath = "/tmp/snortSock.sock"
 
-## Global Snort Running Variable
+## Global Snort Running Variable: just here to tell if snort is running or not
 isSnort = 0
+
+## Very simple file display for file manager, modified to show folders, subdirs, and internal files
+def fileManager(filePath):
+        fileList = []
+        filePath = os.path.abspath(filePath)
+        for dirPath, dirNames, fileNames in os.walk(filePath):
+                if os.path.abspath(dirPath) == filePath:
+                        for dirName in dirNames:
+                                fileList.append(os.path.join(dirPath, dirName) + '/')
+                        for fileName in fileNames:
+                                fileList.append(os.path.join(dirPath, fileName))
+                break
+        return fileList
 
 ### Internal commands are for when there is no web request
 ## Internal Kill Snort commant
@@ -50,11 +63,16 @@ def intStartSnort():
 # DEfine flask app name
 app = Flask(__name__)
 
-## Path to main page, holds most down low cotnrol
+## Path to main page, just index
 @app.route('/')
 def main():
         return render_template('index.html')
 
+
+### Snortpipe Interface Commands
+# start: Start Snort up if there isnt a session already running
+# kill: Kills current snort session aslong as there is a session runnign
+# quit: Quits handler script and kills current snort session if there is one running
 
 ## Starts Snort Session
 # Need to add check to make sure that there is no Snort session currently running
@@ -114,37 +132,37 @@ def reloadSnort():
                 intStartSnort()
         return redirect('/')
 
-### Break for New Dev ###
-
-## Ideas
-# -Edit text file in browser
-# Need to load file and get contents (rw+)
-# Give environment to actually edit text (No idea how, some flask BS)
-# Save file back to machine
-# Restart Snort with new file automatically?
-
 
 ## Shitty text editor
-# Half of this thing is stolenbut it works well enough, need to brush up on HTML to make it look purty
+# like 90% of this is stolen
 @app.route('/edit', methods=['GET', 'POST'])
 def editFile():
-    fileToEdit = request.args.get('file')
+    fileToEdit = request.args.get('file') #Set file to browser argument, handy
     if not fileToEdit or not os.path.isfile(fileToEdit):
-        return "Invalid file path", 406
+        return "Invalid file path", 404
 
     if request.method == 'POST':
         content = request.form.get('content')
-        with open(fileToEdit, 'w') as fp:
-            fp.write(content)
+        with open(fileToEdit, 'w') as filePtr:
+            filePtr.write(content)
         redirect_url = f'/edit?file={fileToEdit}'
         return redirect(redirect_url)
-             
-    with open(fileToEdit, 'r') as fp:
-        content = fp.read()
+
+    with open(fileToEdit, 'r') as filePtr:
+        content = filePtr.read()
 
     return render_template('editor.html', content=content, fileToEdit=fileToEdit)
+
+## File manager idea
+# Could be a second page or could be something on he edit page where you create a new file or something
+# Just going to use a template and url queries to traverse file system depending on there the user is at
+@app.route('/files', methods=['GET'])
+def fileTest():
+        filePath = request.args.get('file') #File path passed in Via URL, can now use redirects
+        prevPath = os.path.dirname(filePath.rstrip('/')) #Tracker for previous path when the user wants to back up, needed to add rstrip for proper directory traversal
+        return render_template('filemanager.html', fileList = fileManager(filePath), path = filePath, len = len(fileManager(filePath)), prevPath = prevPath)
 
 ## Main function
 if __name__ == '__main__':
         startSnortPipe()
-        app.run(host='0.0.0.0', port=5959)
+        app.run(host='0.0.0.0', port=5959
